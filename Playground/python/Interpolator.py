9bfb7b3091d5ba51863f49eb3279a2bf2bb92a89
JialongYu <jy3717@ic.ac.uk>
2020-05-19 12:35:22 +0100

    latest files added

--- Playground/python/Interpolator.py
+++ Playground/python/Interpolator.py
@@ -4,10 +4,13 @@ import time
 import numpy as np
 from io import BytesIO
 from fractions import Fraction
+from motion_estimation import get_motion_vectors_fs
+from tss import get_motion_vectors_tss
 from decimal import Decimal
 from copy import deepcopy
 from Globals import debug_flags
 from VideoStream import BenchmarkVideoStream, VideoStream
+import matplotlib.pyplot as plt
 
 '''
 blends frames
@@ -35,6 +38,9 @@ class BaseInterpolator:
         self.video_stream = None
         self.video_out_writer = None
 
+        self.MV_field_idx= -1 #Index in source video that the current motion field is based on.
+        self.MV_field=[]
+
         self.max_frames_possible = None
         self.rate_ratio = None
 
@@ -59,7 +65,7 @@ class BaseInterpolator:
         if (debug_flags['debug_IO']):
             print(f'Loaded input video from {self.video_in_path}.')
             print(vid.get_meta_data())
-            
+
         self.video_stream = VideoStream(vid, self.max_cache_size)
 
         # maximum frames possible according to target_fps
@@ -72,12 +78,12 @@ class BaseInterpolator:
             raise Exception('video output path not given')
 
 
-            ''' 
+            '''
             See bottom of file for options
             '''
         self.video_out_writer = imageio.get_writer(
             self.video_out_path, fps=self.target_fps, macro_block_size=0, quality=6)
-        
+
 
     def interpolate_video(self):
         if self.video_in_path is None or self.video_out_path is None:
@@ -143,15 +149,205 @@ class BaseInterpolator:
 
 
 '''
+MEMCI using full search for ME and uniderictional
+MCI with median filter for filling holes.
+
+
+
+'''
+
+class MEMCIInterpolator(BaseInterpolator):
+    def __init___(self, target_fps, video_in_path=None, video_out_path=None, max_out_frames=math.inf, max_cache_size=2, **args):
+        super().__init__(target_fps, video_in_path,
+                         video_out_path, max_out_frames, max_cache_size)
+
+
+    def get_interpolated_frame(self, idx):
+
+        #source_frame is the previous frame in the source vidoe.
+        source_frame_idx = math.floor(idx/self.rate_ratio)
+        source_frame = self.video_stream.get_frame(source_frame_idx)
+
+        #Normalized distance from current_frame to the source frame.
+        dist = idx/self.rate_ratio - math.floor(idx/self.rate_ratio)
+
+        #If the frame to be interpolated is coinciding with a source frame.
+        if dist == 0:
+            return source_frame
+
+
+        #Check if the frame to be interpolated is between the two frames
+        #that the current motion field is estimated on.
+        if not self.MV_field_idx < idx/self.rate_ratio < self.MV_field_idx+1:
+            block_size = 10
+            steps = 6
+            target_frame = self.video_stream.get_frame(source_frame_idx+1)
+            self.MV_field = get_motion_vectors_fs(block_size, steps, source_frame, target_frame)
+            # self.MV_field = get_motion_vectors_tss(block_size,steps, source_frame, target_frame)
+            self.MV_field_idx = source_frame_idx
+
+
+            #Uncomment if you want to plot vector field when running benchmark.py
+            #self.plot_vector_field(block_size,steps, source_frame)
+
+
+
+
+        #Initialize new frame
+        Interpolated_Frame =  np.ones(source_frame.shape)*-1
+        #Matrix with lowest sad value fo every interpolated pixel.
+        SAD_interpolated_frame = np.full([source_frame.shape[0],source_frame.shape[1]],np.inf)
+
+        #Follow motion vectorcs to obtain interpolated pixel Values
+        #If interpolated frame has multiple values, take the one with lowest SAD.
+        for u in range(0, source_frame.shape[0]):
+            for v in range(0, source_frame.shape[1]):
+
+                #Get the new coordinates by following scaled MV.
+                u_i = int(u + round(self.MV_field[u,v,0]*dist))
+                v_i = int(v + round(self.MV_field[u,v,1]*dist))
+
+                if  self.MV_field[u,v,2] <= SAD_interpolated_frame[u_i, v_i]:
+
+                    Interpolated_Frame[u_i, v_i] =  source_frame[u, v]
+                    SAD_interpolated_frame[u_i, v_i] = self.MV_field[u,v,2]
+
+
+
+        #Run median filter over empty pixels in the interpolated frame.
+        k=10 #Median filter size = (2k+1)x(2k+1)
+        #Bad implementation. Did not find any 3d median filter
+        # that can be applied to specific pixels.
+        New_Interpolated_Frame = np.copy(Interpolated_Frame)
+        for u in range(0, Interpolated_Frame.shape[0]):
+            for v in range(0, Interpolated_Frame.shape[1]):
+                if Interpolated_Frame[u,v,0] == -1:
+                    u_min=max(0,u-k)
+                    u_max=min(Interpolated_Frame.shape[0],u+k+1)
+                    v_min=max(0,v-k)
+                    v_max=min(Interpolated_Frame.shape[1],v+k+1)
+                    New_Interpolated_Frame[u,v,0] = np.median(Interpolated_Frame[u_min:u_max,v_min:v_max,0])
+                    New_Interpolated_Frame[u,v,1] = np.median(Interpolated_Frame[u_min:u_max,v_min:v_max,1])
+                    New_Interpolated_Frame[u,v,2] = np.median(Interpolated_Frame[u_min:u_max,v_min:v_max,2])
+
+        New_Interpolated_Frame = New_Interpolated_Frame.astype(source_frame.dtype)
+        return New_Interpolated_Frame
+
+
+    def plot_vector_field(self,block_size,steps,source_frame):
+        #Downsample so each vector represents one block.
+
+        Down_sampled_MV=self.MV_field[::block_size,::block_size,:]
+        X, Y = np.meshgrid(np.linspace(0,self.MV_field.shape[1]-1, Down_sampled_MV.shape[1]), \
+                            np.linspace(0,self.MV_field.shape[0]-1, Down_sampled_MV.shape[0]))
+
+        U = Down_sampled_MV[:,:,0]  #X-direction
+        V = Down_sampled_MV[:,:,1]  #Y-direction
+        M = np.hypot(U, V)          #Magnitude of vector.
+
+        fig, ax = plt.subplots(1,1)
+        source_image = ax.imshow(source_frame)
+        vector_field = ax.quiver(X, Y, U, V ,M,cmap='coolwarm', angles='uv',units='x', pivot='tip', width=1,
+                       scale=1 / 0.5)
+
+        cbar=fig.colorbar(vector_field)
+        cbar.ax.set_ylabel('|MV| in pixels')
+        plt.title("Block size="+str(block_size)+ "\nSteps="+str(steps))
+        plt.show()
+
+    def __str__(self):
+        return 'MEMCI'
+
+'''
 %
 %   e.g. 24->60
-%   A A A B B C C C D D
+%   A A A B B C C    C D D
 %
 %   e.g. 25->30
 %   A A B C D E F
 %
 '''
 
+
+class Bi(BaseInterpolator):
+    def __init___(self, target_fps, video_in_path=None, video_out_path=None, max_out_frames=math.inf, max_cache_size=2,
+                  **args):
+        super().__init__(target_fps, video_in_path,
+                         video_out_path, max_out_frames, max_cache_size)
+
+    def get_interpolated_frame(self, idx):
+
+        source_frame_idx = math.floor(idx / self.rate_ratio)
+        source_frame = self.video_stream.get_frame(source_frame_idx)
+
+        dist = idx / self.rate_ratio - math.floor(idx / self.rate_ratio)
+
+        if dist == 0:
+            return source_frame
+
+        if not self.MV_field_idx < idx / self.rate_ratio < self.MV_field_idx + 1:
+            target_frame = self.video_stream.get_frame(source_frame_idx + 1)
+            # self.MV_field = get_motion_vectors(4, 10, source_frame, target_frame)
+            self.MV_field_idx = source_frame_idx
+
+            '''
+            fwd = get_motion_vectors(4, 10, source_frame, target_frame)
+            bwd = get_motion_vectors(4, 10, target_frame, source_frame)
+
+            if fwd[,,2]>bwd[,,2]: #use the one with smaller SAD
+                self.MV_field = bwd
+                dist = 1 - dist
+            else :
+                self.MV_field = fwd
+
+            self.MV_field_idx = source_frame_idx
+            '''
+            fwd = get_motion_vectors_tss(4, 3, source_frame, target_frame)
+            bwd = get_motion_vectors_tss(4, 3, target_frame, source_frame)
+            self.MV_field = fwd
+        Interpolated_Frame = np.ones(source_frame.shape, dtype='float64') * -1
+        SAD_interpolated_frame = np.full([source_frame.shape[0], source_frame.shape[1]], np.inf)
+
+        for u in range(0, source_frame.shape[0]):
+            for v in range(0, source_frame.shape[1]):
+                if fwd[u, v, 2] > bwd[u, v, 2]:
+                    dist = 1.0 - dist
+                    u_i = int(u + round(bwd[u, v, 0] * dist))
+                    v_i = int(v + round(bwd[u, v, 1] * dist))
+
+                    if bwd[u, v, 2] <= SAD_interpolated_frame[u_i, v_i]:
+                        Interpolated_Frame[u_i, v_i] = source_frame[u, v]
+                        SAD_interpolated_frame[u_i, v_i] = bwd[u, v, 2]
+                        self.MV_field[u, v] = bwd[u, v]
+
+                        # self.MV_field[u,v,0] = bwd[u,v,0]
+                        # self.MV_field[u,v,1] = bwd[u,v,1]
+                        # self.MV_field[u,v,2] = bwd[u,v,2]
+
+                else:
+                    u_i = int(u + round(fwd[u, v, 0] * dist))
+                    v_i = int(v + round(fwd[u, v, 1] * dist))
+
+                    if fwd[u, v, 2] <= SAD_interpolated_frame[u_i, v_i]:
+                        Interpolated_Frame[u_i, v_i] = source_frame[u, v]
+                        SAD_interpolated_frame[u_i, v_i] = fwd[u, v, 2]
+
+        k = 5  # Median filter size = (2k+1)x(2k+1)
+        for u in range(0, Interpolated_Frame.shape[0]):
+            for v in range(0, Interpolated_Frame.shape[1]):
+                if Interpolated_Frame[u, v, 0] == -1:
+                    u_min = max(0, u - k)
+                    u_max = min(Interpolated_Frame.shape[0], u + k + 1)
+                    v_min = max(0, v - k)
+                    v_max = min(Interpolated_Frame.shape[1], v + k + 1)
+                    Interpolated_Frame[u, v, 0] = np.median(Interpolated_Frame[u_min:u_max, v_min:v_max, 0])
+                    Interpolated_Frame[u, v, 1] = np.median(Interpolated_Frame[u_min:u_max, v_min:v_max, 1])
+                    Interpolated_Frame[u, v, 2] = np.median(Interpolated_Frame[u_min:u_max, v_min:v_max, 2])
+
+        return Interpolated_Frame
+
+    def __str__(self):
+        return 'BI'
 
 class NearestInterpolator(BaseInterpolator):
     def __init___(self, target_fps, video_in_path=None, video_out_path=None, max_out_frames=math.inf, max_cache_size=2, **args):
@@ -188,8 +384,8 @@ class OversampleInterpolator(BaseInterpolator):
                          video_out_path, max_out_frames, max_cache_size)
 
     def get_interpolated_frame(self, idx):
-        output = []
 
+        output = []
         # this period is the number of frame in the targetRate
         # before a cycle occurs (e.g. in the 24->60 case it occurs between B &
         # C at period = 5
@@ -216,7 +412,6 @@ class OversampleInterpolator(BaseInterpolator):
                 print(f'targetframe: {idx}, using source frame: {frame_num}')
 
             output = self.video_stream.get_frame(frame_num)
-
         else:
             frameA_idx = math.floor(
                 key_frame_idx / self.rate_ratio + rate_ratios_from_key_frame)
@@ -245,7 +440,7 @@ class OversampleInterpolator(BaseInterpolator):
 '''
 %
 %   e.g. 24->60 (rateRatio 2.5, period 5)
-%   A (1.5A+B)/2.5 (0.5A+2B)/2.5 (2B+0.5C)/2.5 (B+1.5C)/2.5 
+%   A (1.5A+B)/2.5 (0.5A+2B)/2.5 (2B+0.5C)/2.5 (B+1.5C)/2.5
 %
 '''
 
@@ -301,9 +496,11 @@ class LinearInterpolator(BaseInterpolator):
 
 
 InterpolatorDictionary = {
-    'nearest': NearestInterpolator,
-    'oversample': OversampleInterpolator,
-    'linear': LinearInterpolator
+    # 'MEMCI' : MEMCIInterpolator,
+    'BI':Bi
+    #'nearest': NearestInterpolator,
+    #'oversample': OversampleInterpolator,
+    #'linear': LinearInterpolator
 }
 
 
@@ -355,4 +552,3 @@ Parameters for saving
         can't decode videos that are odd in size and some codecs will produce
         poor results or fail. See https://en.wikipedia.org/wiki/Macroblock.
     '''
-    
---
+++ Playground/python/tss.py
@@ -0,0 +1,79 @@
+import numpy as np
+import math
+from imageio import imread, imwrite
+import sys
+import cProfile
+import time
+
+def get_motion_vectors_tss(block_size, steps, source_frame, target_frame):
+    output = np.empty_like(source_frame, dtype='float32')
+    prec_dic = [1, 2, 1, 2, 3, 2, 1, 2, 1]
+
+    prev_time = time.time()
+    for s_row in range(0, source_frame.shape[0], block_size):
+        #print(s_row, time.time() - prev_time)
+        #prev_time = time.time()
+        for s_col in range(0, source_frame.shape[1], block_size):
+            source_block = source_frame[s_row:s_row+block_size, s_col:s_col+block_size, :]
+            curr_block_size = source_block[:,:,0].shape
+            nr_of_pxl=curr_block_size[0]*curr_block_size[1]
+            center_sad = None
+            lowest_sad = 9999999999999
+            lowest_idx = (0, 0)
+            lowest_i = 0
+            curr_row = s_row
+            curr_col = s_col
+            for step in range(steps, 0, -1):
+                S = 2 ** (step - 1)
+                i = -1
+                for t_row in range(curr_row - S, curr_row + S + 1, S):
+                    for t_col in range(curr_col - S,curr_col + S + 1, S):
+                        i += 1
+                        if (t_row != curr_row or t_col != curr_col or center_sad == None) and (t_row >= 0 and t_row <= source_frame.shape[0] - block_size and t_col >= 0 and t_col <= source_frame.shape[1] - block_size):
+                            target_block = target_frame[t_row:t_row+curr_block_size[0], t_col:t_col+curr_block_size[1], :]
+                            sad = np.sum(np.abs(np.subtract(source_block, target_block)))
+
+                            if sad < lowest_sad or (sad == lowest_sad and prec_dic[i] > prec_dic[lowest_i]):
+                                lowest_sad = sad
+                                lowest_idx = (t_row, t_col)
+                                lowest_i = i
+                curr_row = lowest_idx[0]
+                curr_col = lowest_idx[1]
+                center_sad = lowest_sad
+            block = np.full((curr_block_size[0], curr_block_size[1], 3), [lowest_idx[0] - s_row, lowest_idx[1] - s_col, lowest_sad])
+            output[s_row:s_row+curr_block_size[0], s_col:s_col+curr_block_size[1], :] = block
+
+    return output
+
+if __name__ == "__main__":
+    if sys.argv[1] == '-f':
+        csv_path = sys.argv[2]
+        image_height = int(sys.argv[3])
+        image_width = int(sys.argv[4])
+        out_path = sys.argv[5]
+        output = np.genfromtxt(csv_path, delimiter=',').reshape((image_height, image_width, 3))
+    else:
+        block_size = int(sys.argv[1])
+        steps = int(sys.argv[2])
+        im1 = imread(sys.argv[3])[:,:,:3]
+        im2 = imread(sys.argv[4])[:,:,:3]
+        out_path = sys.argv[5]
+        output = get_motion_vectors(block_size, steps, im1, im2)
+        np.savetxt(out_path + "/out.csv", output.reshape(-1), delimiter=',')
+
+    output_intensity = np.copy(output)
+    max_intensity = 0
+    for i in range(output_intensity.shape[0]):
+        for j in range(output_intensity.shape[1]):
+            intensity = float(output_intensity[i,j,0]) ** 2.0 + float(output_intensity[i,j,1]) ** 2.0
+            if intensity > max_intensity:
+                max_intensity = intensity
+            output_intensity[i,j,:] = [intensity, intensity, intensity]
+    output_intensity = 255 - (output_intensity * (255.0 / float(max_intensity)))
+    imwrite(out_path + "/out_intensity.png", output_intensity)
+
+
+
+# im1 = imread("../interpolation-samples/still_frames/eg1/frame1.png")[:,:,:3]
+# im2 = imread("../interpolation-samples/still_frames/eg1/frame2.png")[:,:,:3]
+# cProfile.run('get_motion_vectors(40, 300, im1, im2)')
