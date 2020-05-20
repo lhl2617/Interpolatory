f28dbed5a1ec5681199cb74440aa7512abbd368d
Olly Larkin <ollylemonlarkin@gmail.com>
2020-05-20 15:58:50 +0100

    full search should now work with any block size

--- Playground/python/ME/full_search.py
+++ Playground/python/ME/full_search.py
@@ -11,22 +11,23 @@ block_size = size of block used in source and target frame
 target_region = number of pixels padding the block to be searched
 '''
 def get_motion_vectors(block_size, target_region, source_frame, target_frame):
-    output = np.empty_like(source_frame, dtype='float32')
-    lowest_sad_const = block_size * block_size * (255 + 255 + 255) + 1    # maximum sad score for a block
-    lowest_distance_const = block_size * block_size
+    lowest_sad_const = 999999999999    # maximum sad score for a block
+    lowest_distance_const = 999999999999
 
-    # prev_time = time.time()
+    source_frame_pad = np.pad(source_frame, ((0,block_size), (0,block_size), (0,0)))  # to allow for non divisible block sizes
+    target_frame_pad = np.pad(target_frame, ((0,block_size), (0,block_size), (0,0)))
+    
+    output = np.empty_like(source_frame_pad, dtype='float32')
+
     for s_row in range(0, source_frame.shape[0], block_size):
-        # print(s_row, time.time() - prev_time)
-        # prev_time = time.time()
         for s_col in range(0, source_frame.shape[1], block_size):
-            source_block = source_frame[s_row:s_row+block_size, s_col:s_col+block_size, :]
+            source_block = source_frame_pad[s_row:s_row+block_size, s_col:s_col+block_size, :]
             lowest_sad = lowest_sad_const
             lowest_distance = lowest_distance_const
             target_index = (0, 0)
-            for t_row in range(max(0, s_row - target_region), min(target_frame.shape[0] - block_size, s_row + target_region + 1)):
-                for t_col in range(max(0, s_col - target_region), min(target_frame.shape[1] - block_size, s_col + target_region + 1)):
-                    target_block = target_frame[t_row:t_row+block_size, t_col:t_col+block_size, :]
+            for t_row in range(max(0, s_row - target_region), min(target_frame_pad.shape[0] - block_size, s_row + target_region + 1)):
+                for t_col in range(max(0, s_col - target_region), min(target_frame_pad.shape[1] - block_size, s_col + target_region + 1)):
+                    target_block = target_frame_pad[t_row:t_row+block_size, t_col:t_col+block_size, :]
                     sad = np.sum(np.abs(np.subtract(source_block, target_block)))
                     distance = (s_row - t_row) *  (s_row - t_row) + (s_col - t_col) * (s_col - t_col)
                     if sad < lowest_sad or (sad == lowest_sad and distance < lowest_distance):
@@ -36,7 +37,7 @@ def get_motion_vectors(block_size, target_region, source_frame, target_frame):
             block = np.full((block_size, block_size, 3), [target_index[0] - s_row, target_index[1] - s_col, lowest_sad])
             output[s_row:s_row+block_size, s_col:s_col+block_size, :] = block
     
-    return output
+    return output[:source_frame.shape[0], :source_frame.shape[1], :]
 
 if __name__ == "__main__":
     if sys.argv[1] == '-f':
