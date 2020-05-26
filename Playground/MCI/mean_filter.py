81f74811730cac51f2d01598d7d677bee2f0e5c3
BBBBBbruce <bubblebruce@outlook.com>
2020-05-26 20:22:28 +0800

    reduce redundency

--- Playground/MCI/mean_filter.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-
-def mean_filter(block):
-    mean = np.mean(block, axis=(0,1))
-    return (mean[0], mean[1])
--- Playground/python/testsuite.sh
+++
@@ -1,7 +0,0 @@
-echo "I am writing a testsuite"
-echo "what?"
-
-echo "default case"
-python ./benchmark.py
-
-$SHELL
