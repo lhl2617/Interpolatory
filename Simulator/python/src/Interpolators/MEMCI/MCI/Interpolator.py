91e8464e047c66ad4e9fd2809d4e8a333d94c4b9
JialongYu <jy3717@ic.ac.uk>
2020-06-04 15:38:52 +0100

    changed default MCI method from bi to uni2

--- Simulator/python/src/Interpolators/MEMCI/MCI/Interpolator.py
+++ Simulator/python/src/Interpolators/MEMCI/MCI/Interpolator.py
@@ -433,7 +433,7 @@ class BiDirInterpolator(BaseInterpolator):
         return 'bwMEMCI'
 
 def MEMCI (target_fps, video_in_path=None, video_out_path=None, max_out_frames=math.inf, max_cache_size=2, **args):
-    mci_mode = 'bidir'
+    mci_mode = 'unidir2'
 
     if 'mci_mode' in args:
         mci_mode = args['mci_mode']
